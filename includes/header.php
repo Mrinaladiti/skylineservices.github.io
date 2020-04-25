f8d434bbaafc58edf3c48bdd0e4a7326d9cf9510
Mrinal Chakraborty <mrinalchakraborty1509@gmail.com>
2020-04-24 21:01:05 -0700

    publishing website
    wow,i am very happy.

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar"><div class="img_home_pos">
+      <div class="container">
+
+         <marquee> <a href="everyone.php"><img src="images/logo.jpg" height="90" </div>
+
+      <a class="navbar-brand" href="index.php">SKYLINE   Online Sevices & Management System</a></marquee>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">Services</a></li>
+            
+            <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+           
+            <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+            
+            <li class="nav-item"><a href="admin/index.php" class="nav-link">Admin</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
